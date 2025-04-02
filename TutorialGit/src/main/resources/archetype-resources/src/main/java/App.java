17b3a8be089ac0e44c9647b3223eb2f1ac570c1a
jjgomezrosado <44209806+jjgomezrosado@users.noreply.github.com>
2025-04-02 19:41:29 +0200

    [ADD] Añadir función esPar
    Añadida función esPar

--- TutorialGit/src/main/resources/archetype-resources/src/main/java/App.java
+++ TutorialGit/src/main/resources/archetype-resources/src/main/java/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
+
+
         System.out.println( "Hello World!" );
+
+
+    }
+
+    public static boolean esPar(int n) {
+        return n % 2 == 0;
     }
 }
